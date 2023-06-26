461b47d6a3c410818fce661d6bd443f131f0fd53
Chris J. Karr <chris@audacious-software.com>
2023-06-26 18:04:52 +0000

    Updating URL config for testing

--- .circleci/circle_settings.py
+++ .circleci/circle_settings.py
@@ -41,7 +41,7 @@ MIDDLEWARE = (
     'django.middleware.security.SecurityMiddleware',
 )
 
-ROOT_URLCONF = 'simple_detoxify/.circleci/.circle_urls'
+ROOT_URLCONF = 'simple_site/urls.py'
 
 TEMPLATES = [
     {
